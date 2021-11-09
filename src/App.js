95e6559f21845140d2589351231bd4cdd23548a9
YuEunSol <dmsthf209@kookmin.ac.kr>
2021-11-09 20:26:21 +0900

    Header style 수정 Award 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import react from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Story from "./pages/Story";
+import Award from "./pages/Award";
 import Intro from "./pages/Intro";
 import Interpret from "./pages/Interpret";
 import Production from "./pages/Production";
 import Credit from './pages/Credit'
 import Header from './components/common/Header'
 
-const AppStyle = {
-  padding : "1%",
+const BodyStyle = {
+    display : "flex",
 }
 
 
 function App (){
   return (
-  <div style={AppStyle}>
-    
+  <div style={BodyStyle}>
     <BrowserRouter>
     <Header/>
-    <hr/>
       <Routes>
           <Route exact path="/intro" element={<Intro/>}/>
           <Route exact path="/story" element={<Story/>}/>
+          <Route exact path="/award" element={<Award/>}/>
           <Route exact path="/interpret" element={<Interpret/>}/>
           <Route exact path="/production" element={<Production/>}/>
           <Route exact path="/credit" element={<Credit/>}/>
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,20 +2,36 @@ import react from 'react';
 import {Link} from 'react-router-dom'
 
 const LinkStyle = {
-  border : "2px black solid",
-  margin : "10px",
-  padding : "1px",
-  backgroundColor : "grey"
+  backgroundColor : "#DB202C",
+    display : "flex",
+    justifyContent : "center",
+    alignItems : "center",
+    flexDirection : "column",
+    color : "white",
+  fontFamily: "Roboto Mono",
+    fontStyle : "bold",
+    width : "16.67vw",
+    height : "9.15vh",
+    textDecoration : "none",
+}
+
+const HeaderNum = {
+    fontSize : "0.8rem",
+}
+
+const HeaderTitle = {
+  fontSize: "1.3rem",
 }
 
 const Header = () => {
   return (
     <>
-      <Link to="/intro" style={LinkStyle}>Intro</Link>
-      <Link to="/story" style={LinkStyle} >Story</Link>
-      <Link to="/interpret" style={LinkStyle}>Interpret</Link>
-      <Link to="/production" style={LinkStyle}>Production</Link>
-      <Link to="/credit" style={LinkStyle}>Credit</Link>
+        <Link to="/intro" style={LinkStyle}><h style={HeaderNum}>I.</h><h style={HeaderTitle}>Intro</h></Link>
+      <Link to="/story" style={LinkStyle} ><h style={HeaderNum}>II.</h><h style={HeaderTitle}>Story</h></Link>
+        <Link to="/award" style={LinkStyle}><h style={HeaderNum}>III.</h><h style={HeaderTitle}>Award</h></Link>
+        <Link to="/production" style={LinkStyle}><h style={HeaderNum}>IV.</h><h style={HeaderTitle}>Production</h></Link>
+        <Link to="/interpret" style={LinkStyle}><h style={HeaderNum}>V.</h><h style={HeaderTitle}>Interpret</h></Link>
+      <Link to="/credit" style={LinkStyle}><h style={HeaderNum}>VI.</h><h style={HeaderTitle}>Credit</h></Link>
       
     </>
   )
